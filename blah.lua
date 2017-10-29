dcf342d0d8bc05bc6810dd4fd26e1a9796de287e
Rulerofdabacon <rulerofdabacon@gmail.com>
2017-10-28 19:36:28 -0700

    Elec. furnace update 1
    First update for electric furnaces. Adds them to first logistics tech. Unfortunately still in original research as well.

--- blah.lua
+++ blah.lua
@@ -1,3 +1,6 @@
 data.raw["technology"]["landfill"].unit.ingredients = {
 {"science-pack-1", 1}
 }
+
+
+table.insert(data.raw.technology["logistics"].effects,{type = "unlock-recipe", recipe = "electric-furnace"})
