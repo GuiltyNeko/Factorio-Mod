fc32c5024f611f233bb2bc78892c81d5caa8970b
Rulerofdabacon <rulerofdabacon@gmail.com>
2017-10-28 19:52:10 -0700

    Circuit network update
    Makes the circuit network tech not require green anymore. Just the red.

--- blah.lua
+++ blah.lua
@@ -1,6 +1,10 @@
 data.raw["technology"]["landfill"].unit.ingredients = {
-{"science-pack-1", 1}
+	{"science-pack-1", 1}
 }
 
 
 table.insert(data.raw.technology["logistics"].effects,{type = "unlock-recipe", recipe = "electric-furnace"})
+
+data.raw["technology"]["landfill"].unit.ingredients = {
+	{"science-pack-1", 1}
+}
