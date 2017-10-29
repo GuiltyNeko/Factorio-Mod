64788da655a3b2bf624d31a3425ba34bf902d7a0
Rulerofdabacon <rulerofdabacon@gmail.com>
2017-10-29 04:58:37 -0700

    1.0.11
    Removed Electric furnace recipe from advanced material processing 2 tech.

--- prototypes/blah.lua
+++ prototypes/blah.lua
@@ -14,3 +14,12 @@ if mods["5dim_automatization"] then
   	{"science-pack-1", 1}
 }
 end
+
+
+for i, effect in pairs(data.raw.technology["advanced-material-processing-2"].effects) do
+      if effect.type == "unlock-recipe" and effect.recipe == "electric-furnace" then
+        table.remove(data.raw.technology["advanced-material-processing-2"].effects,i)
+    end
+end
+
+--for i, ingredient in pairs(data.raw.recipe["electric-furnace"]) do --put off for now. Will address later.
