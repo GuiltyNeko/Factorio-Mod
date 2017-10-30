75c1802d03b66ceee475c590a72af075210f98f8
GuiltyNeko <rulerofdabacon@gmail.com>
2017-10-30 01:49:58 -0700

    Electric furnace positioning change
    I moved the electric furnace from data-updates to data.lua, as from my understanding, factorio loads data first, then data-updates. Because the electric furnace fix currently completely overwrites the recipe, any mod that loads in the data-updates phase will be able to change the recipe as they see fit, instead of the possibility of having mine kill their recipe and/or balance.

--- data-updates.lua
+++ data-updates.lua
@@ -1,5 +1,4 @@
 require ("prototypes.5d-loaders")
 require ("prototypes.circuit-network")
-require ("prototypes.electric-furnace")
 require ("prototypes.landfill")
 require ("prototypes.circuit-updates")
