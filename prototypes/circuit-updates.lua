3bb90269d2324e79656c623cc2a88733136eecab
GuiltyNeko <rulerofdabacon@gmail.com>
2017-10-31 14:20:05 -0700

    1.0.21
    Bugfixes

--- prototypes/circuit-updates.lua
+++ prototypes/circuit-updates.lua
@@ -5,19 +5,21 @@ if settings.startup["circuit-updates"].value or settings.startup["everything"].v
 			if recipe.ingredients then
 				recipe.ingredients = {
 				{"burner-inserter", 1},
-      			{"basic-circuit", 1}
+      			{"basic-circuit-board", 1}
 				}
 			end
 		end
 
 		if mods["hacked-splitters"] then
-			local recipe = data.raw.recipe["hacked-splitter"]
-			if recipe.ingredients then
-				recipe.ingredients = {
-				{"splitter", 1},
-     	    	{"copper-cable", 1},
-     	    	{"basic-circuit", 1}
-				}
+			if data.raw.recipe["hacked-splitter"] then
+				local recipe = data.raw.recipe["hacked-splitter"]
+					if recipe.ingredients then
+					recipe.ingredients = {
+					{"splitter", 1},
+     	 		   	{"copper-cable", 1},
+     	 		   	{"basic-circuit-board", 1}
+					}
+				end
 			end
 		end
 	end
