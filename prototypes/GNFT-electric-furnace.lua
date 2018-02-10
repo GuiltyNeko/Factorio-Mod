62a187ea550fad34e03b023ee7b2bd171de2d25e
GuiltyNeko <guiltysoulneko@gmail.com>
2018-02-10 10:33:39 -0800

    1.0.27

--- prototypes/GNFT-electric-furnace.lua
+++ prototypes/GNFT-electric-furnace.lua
@@ -4,7 +4,18 @@ if settings.startup["GNFT-electric-furnace-tech"].value then
 	for i, effect in pairs(data.raw.technology["advanced-material-processing-2"].effects) do						--Remove electric furnace from advanced material processing 2
     	if effect.type == "unlock-recipe" and effect.recipe == "electric-furnace" then
         	table.remove(data.raw.technology["advanced-material-processing-2"].effects,i)
-    	end
+		end
+	end
+
+	if mods.["mini-machines"]then
+		if not settings.startup["mini-tech"].value then
+			table.insert(data.raw.technology["logistics"].effects,{type = "unlock-recipe", recipe = "mini-furance-1"})
+		end
+
+	for i, effect in pairs(data.raw.technology["advanced-material-processing-2"].effects) do						--Remove mini electric furnace from advanced material processing 2
+		if effect.type == "unlock-recipe" and effect.recipe == "mini-furnace-1" then
+			table.remove(data.raw.technology["advanced-material-processing-2"].effects,i)
+		end
 	end
 end
 
@@ -14,21 +25,21 @@ if settings.startup["GNFT-electric-furnace-recipe"] then
 		recipe.ingredients = {
 			{"electronic-circuit", 5},
 			{"steel-plate", 10},
-			{"stone-brick", 10} 
+			{"stone-brick", 10}
 		}
 	end
 	if recipe.normal then
 		recipe.normal.ingredients = {
 			{"electronic-circuit", 5},
 			{"steel-plate", 10},
-			{"stone-brick", 10} 
+			{"stone-brick", 10}
 		}
 	end
 	if recipe.expensive then
 		recipe.expensive.ingredients = {
 			{"electronic-circuit", 5},
 			{"steel-plate", 10},
-			{"stone-brick", 10} 
+			{"stone-brick", 10}
 		}
 	end
 end
